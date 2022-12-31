d08af60c1bcabde23c275a2a935212f0f99d75ae
root <root@Kuro.localdomain>
2023-01-01 00:05:11 +0200

    added slider and pros section, still working on sliders functionality

---
+++ slider.js
@@ -0,0 +1,8 @@
+const nextBtn = document.getElementById("next__slide");
+const slider = document.querySelector(".main__slider");
+
+
+// nextBtn.addEventListener('click', (e) => {
+//     console.log(slider.offsetWidth);
+//     slider.style.left = 270 + e.target.offsetWidth + 'px';
+// })
